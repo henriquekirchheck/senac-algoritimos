eca10c32147233589a126b2b41f54e2315d2c956
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-06-04 17:16:15 -0300

    update tabuada

--- src/tabuada.rs
+++ src/tabuada.rs
@@ -1,14 +1,12 @@
 use utils::inputln;
 
 fn main() {
-    let input = inputln!("Escolha um numero: ");
-    let trimmed_input = input.trim();
-    let number = trimmed_input.parse::<usize>().unwrap();
+    let input = inputln!(<usize>! "Escolha um numero: ");
     for n in 1..=10 {
         println!(
-            "{number} * {n:2} = {:n_size$}",
-            number * n,
-            n_size = trimmed_input.len() + 1
+            "{input} * {n:2} = {:n_size$}",
+            input * n,
+            n_size = input.to_string().len() + 1
         )
     }
 }
